6ba7235c1bd7923b6d1709dbf6975b113c8d143d
Emil Oja <emil.oja@gmail.com>
2020-11-25 06:42:32 +0100

    chore: remove unused code

--- src/components/forms/TranslationForm.js
+++ src/components/forms/TranslationForm.js
@@ -1,35 +1,13 @@
 import React from 'react';
-// import { translate } from '../../canvas/canvas.builder';
 import Canvas from '../../canvas/canvas.builder';
 
 const TranslationForm = props => {
 
-    // const [input, setInput] = useState('');
-
-    // const onTranslationClicked = () => {
-    //      translate(input);
-        
-    // }
-
-    // const onInputChanged = ev => setInput(ev.target.value.trim());
-
     return (
-        // <form>
-        //     <div>
-        //         <label> The letters to translate: </label>
-        //         <input type="text" placeholder="Enter letters" onChange={onInputChanged} />
-        //     </div>
-
-
-        //     <div>
-        //         <button type="button" onClick={onTranslationClicked}>Translate</button>
-        //     </div>
-        //     <Canvas/>
-        // </form>
         <div>
             <Canvas/>
         </div>
-        
+       
     );
 }
 
