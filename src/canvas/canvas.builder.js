2eea344821db412a724319315989ec950d0c09ea
Emil Oja <emil.oja@gmail.com>
2020-11-25 06:35:23 +0100

    chore: translation done

--- src/canvas/canvas.builder.js
+++ src/canvas/canvas.builder.js
@@ -1,78 +1,64 @@
-// import React, { useState } from 'react';
-import React from 'react';
+import React, { Component, } from 'react';
 import signSheet from '../../src/sign-spritesheet-1200.png';
+import {translate} from './inputconverter';
 
-// const [signs, setSigns] = useState([[0, 0], [150, 0], [300, 0]]);
-// eslint-disable-next-line
-const updateCanvas = (signs) => {
-    const canvasRef = React.createRef();
+export default class CanvasCreator extends Component {
 
-    const ctx = canvasRef.current.getContext('2d');
-    const canvasWidth = signs.length * 150;
-
-    let imageObj1 = new Image();
-    imageObj1.src = signSheet
-    imageObj1.onload = function () {
-
-        let i;
-        let targetPosition = 0;
-
-        for (i = 0; i < signs.length; i++) {
-
-            ctx.drawImage(imageObj1, signs[i][0], signs[i][1], 150, 150, targetPosition, 0, 150, 150);
-            // objekt     source X    source Y   sHeight/width targetx,target y  
-            targetPosition += 150;
+    constructor(props) {
+        super(props);
+        this.canvasRef = React.createRef();
+        this.state = {
+            canvasWidth: null,
+            input: null
         }
     }
-    return (
-        <canvas ref={this.canvasRef} width={canvasWidth} height={150}> </canvas>
-    );
-}
 
-export const translate = (input) => {
+    onTranslationClicked () {
+        console.log(this.state.input);
+        this.updateCanvas(translate(this.state.input));
+   }
 
-    
-    let letterCordinates = [];
+   handleChange (e) {
+    this.setState({input: e.target.value.trim().toLowerCase()});
+  }
 
-    for (var i in input) {
-        
-        letterCordinates.push(getSigns(input.charAt(i)));
-      }
+    updateCanvas(signs) {
+        this.setCanvaswidth(signs.length * 150);
+        const ctx = this.canvasRef.current.getContext('2d');
+        let imageObj1 = new Image();
+        imageObj1.src = signSheet
+        imageObj1.onload = function () {
 
-    console.log(letterCordinates);
-   // updateCanvas(letterCordinates);
+            let i;
+            let targetPositionY = 0;
 
-}
-   const getSigns = (letter) => {
+            for (i = 0; i < signs.length; i++) {
 
-    let signs = new Map();
-   
-    signs.set('a',[0,0]);
-    signs.set('b',[150,0]);
-    signs.set('c',[300,0]);
-    signs.set('d',[450,0]);
-    signs.set('e',[600,0]);
-    signs.set('f',[750,0]);
-    signs.set('g',[900,0]);
-    signs.set('h',[1050,0]);
-    signs.set('i',[0,150]);
-    signs.set('j',[150,150]);
-    signs.set('k',[300,150]);
-    signs.set('l',[450,150]);
-    signs.set('m',[600,150]);
-    signs.set('n',[750,150]);
-    signs.set('o',[900,150]);
-    signs.set('p',[1050,150]);
-    signs.set('q',[0,300]);
-    signs.set('r',[150,300]);
-    signs.set('s',[300,300]);
-    signs.set('t',[450,300]);
-    signs.set('u',[600,300]);
-    signs.set('v',[750,300]);
-    signs.set('w',[900,300]);
-    signs.set('x',[1050,300]);
-    signs.set('y',[0,450]);
-    signs.set('z',[150,450]);
-       return  signs.get(letter);
-}
+                ctx.drawImage(imageObj1, signs[i][0], signs[i][1], 150, 150, targetPositionY, 0, 150, 150);
+                targetPositionY += 150;
+            }
+        }
+    }
+    render() {
+        return (
+            <div>
+                <canvas ref={this.canvasRef} width={this.state.canvasWidth} height={150}> </canvas>
+                <form>
+                    <div>
+                        <label> The letters to translate: </label>
+                        <input type="text" placeholder="Enter letters" onChange={(e) => {this.handleChange(e)}} />
+                    </div>
+
+                    <div>
+                        <button type="button" onClick={this.onTranslationClicked.bind(this)}>Translate</button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+    setCanvaswidth(width) {
+        this.setState({ canvasWidth: width });
+    }
+};
+
 
--- src/components/forms/TranslationForm.js
+++ src/components/forms/TranslationForm.js
@@ -1,29 +1,35 @@
-import React, { useState } from 'react';
-import { translate } from '../../canvas/canvas.builder';
+import React from 'react';
+// import { translate } from '../../canvas/canvas.builder';
+import Canvas from '../../canvas/canvas.builder';
 
 const TranslationForm = props => {
 
-    const [input, setInput] = useState('');
+    // const [input, setInput] = useState('');
 
-    const onTranslationClicked = () => {
-        translate(input);
-    }
+    // const onTranslationClicked = () => {
+    //      translate(input);
+        
+    // }
 
-    const onInputChanged = ev => setInput(ev.target.value.trim());
+    // const onInputChanged = ev => setInput(ev.target.value.trim());
 
     return (
-        <form>
-            <div>
-                <label> The letters to translate: </label>
-                <input type="text" placeholder="Enter letters" onChange={onInputChanged} />
-            </div>
+        // <form>
+        //     <div>
+        //         <label> The letters to translate: </label>
+        //         <input type="text" placeholder="Enter letters" onChange={onInputChanged} />
+        //     </div>
 
 
-            <div>
-                <button type="button" onClick={onTranslationClicked}>Translate</button>
-            </div>
-
-        </form>
+        //     <div>
+        //         <button type="button" onClick={onTranslationClicked}>Translate</button>
+        //     </div>
+        //     <Canvas/>
+        // </form>
+        <div>
+            <Canvas/>
+        </div>
+        
     );
 }
 
