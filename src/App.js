801e9d4c6c7cf499f687c367f95fe73abc6c72d8
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2020-11-25 15:48:36 +0100

    Vattenkruka feat: Added a not found page and router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Register from './components/containers/Register';
 import Translation from './components/containers/Translation';
 import History from './components/containers/Translation';
+import NotFound from './components/containers/NotFound';
 import {
   BrowserRouter as Router,
   Switch,
@@ -20,6 +21,7 @@ function App() {
           <Route path="/register" component={Register} />
           <Route path="/translation" component={Translation} />
           <Route path="/history" component={History} />
+          <Route path="*" component={NotFound} />
         </Switch>
     </div>
      </Router>
---
+++ src/components/containers/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => (
+    <div>
+        <h1>Page not Found</h1>
+    </div>
+);
+
+export default NotFound;
